2f973236a7b44474fa159813ff41f9dcc1e24f3d
Lalu Fatan <lalufatan25@gmail.com>
2025-10-16 16:22:24 +0700

    Menambahkan dan mencoba TataletakRowColumn

--- app/src/main/java/com/example/tataletak2/tataletak.kt
+++ app/src/main/java/com/example/tataletak2/tataletak.kt
@@ -78,3 +78,21 @@ fun TataletakColumnRow(modifier: Modifier) {
         }
     }
 }
+
+@Composable
+fun TataletakRowColumn(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        //Kolom1
+        Column(){
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
+        //Kolom2
+        Column(){
+            Text(text = "Komponen1Kolom2")
+            Text(text = "Komponen2Kolom2")
+            Text(text = "Komponen3Kolom2")
+        }
+    }
+}
